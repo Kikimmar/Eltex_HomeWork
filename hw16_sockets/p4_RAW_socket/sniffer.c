89f5cba8b67501b2093c9ddf3d47d7a38d0eecc8
Kikimmar <kikimmar.m@gmail.com>
2025-10-03 15:30:04 +0700

    The full hw, added the RAW-socket programm

---
+++ hw16_sockets/p4_RAW_socket/sniffer.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define MAX_SIZE 65536
+
+int main()
+{
+    int sockfd;
+    char buffer[MAX_SIZE];
+
+    sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
+    if (sockfd < 0)
+    {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1)
+    {
+        ssize_t bytes = recvfrom(sockfd, buffer, MAX_SIZE, 0, NULL, NULL);
+        if (bytes < 0)
+        {
+            perror("recvfrom");
+            close(sockfd);
+            exit(EXIT_FAILURE);
+        }
+
+        printf("Received TCP packet of %zd bytes\n", bytes);
+        // Здесь без разбора заголовков можно обработать или сохранить, если нужно
+    }
+
+    close(sockfd);
+    return 0;
+}
+
