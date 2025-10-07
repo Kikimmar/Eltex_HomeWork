2ff82924047b6a9643067a706c0b0fbf247647ac
kikimmar <kikimmar.m@gmail.com>
2025-10-07 22:12:47 +0700

    started doin hw about Raw_udp

---
+++ hw16_sockets/p4_RAW_udp/client.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <netinet/udp.h>
+
+#define SERVER_IP "127.0.0.1"
+#define SERVER_PORT 12345
+#define BUF_SIZE 1024
+
+#define UDP_HDRLEN 8
+
+int main()
+{
+    int sock_fd;
+    struct sockaddr_in server_addr;
+    char buffer[BUF_SIZE];
+
+    int source_port = 54321;  //придуманный
+    
+    const char *msg = "Hello, server";
+    size_t msg_len = strlen(msg);
+    size_t udp_packet_len = UDP_HDRLEN + msg_len;
+
+    sock_fd = socket(AF_INET, SOCK_RAW, IPPROTO_UDP);
+    if (sock_fd < 0)
+    {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+    if (inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr) != 1)
+    {
+        perror("inet_pton");
+        close(sock_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // UDP header
+    struct udphdr *udp = (struct udphdr *)buffer;
+    udp->source = htons(source_port);
+    udp->dest = htons(SERVER_PORT);
+    udp->len = htons(udp_packet_len);
+    udp->check = 0;
+
+    memcpy(buffer + UDP_HDRLEN, msg, msg_len);
+
+    ssize_t sent_bytes = sendto(sock_fd, buffer, udp_packet_len, 0, 
+            (struct sockaddr*)&server_addr, sizeof(server_addr));
+    if (sent_bytes < 0)
+    {
+        perror("sendto");
+        close(sock_fd);
+        exit(EXIT_FAILURE);
+    }
+    printf("Sent %zd bytes to server.\n", sent_bytes);
+
+    close(sock_fd);
+    return 0;
+}
