5eb80a72c103a6347c283fcb7d25acc9c830fdeb
Kikimmar <kikimmar.m@gmail.com>
2025-12-28 20:28:49 +0700

    Added p2

--- hw20_modules_linux/p2/test2.c
+++ hw20_modules_linux/p2/test2.c
@@ -3,67 +3,86 @@
 #include <linux/fs.h>
 #include <linux/rwlock.h>
 #include <linux/uaccess.h>
-#include <linux/minmax.h>
 
 static int major = 0;
 static rwlock_t lock;
-static char test_string[15] = "Hello!\0";
+static char test_string[16] = "Hello, World!\n";
 
-static ssize_t test_read(struct file *fd, char __user *buff, size_t size, loff_t *off)
+static loff_t test_llseek(struct file *file, loff_t off, int whence)
 {
-    size_t to_copy;
-    
-    if(*off >= 15)
-     return 0;
-
-    to_copy = min_t(size_t, size, 15 - (size_t)*off);
-
-    read_lock(&lock);
-    ssize_t rc = simple_read_from_buffer(buff, to_copy, off, test_string, 15);
-    read_unlock(&lock);
-    
-    return rc;
+    return fixed_size_llseek(file, off, whence, 15);
 }
 
-
-ssize_t test_write(struct file *fd, const char __user *buff, size_t size, loff_t *off)
+static int test_open(struct inode *inode, struct file *file)
 {
-    size_t rc = 0;
+    return 0;
+}
+
+static ssize_t test_read(struct file *file, char __user *buff, size_t size, loff_t *off)
+{
+    int len = strlen(test_string);
+    
+    if (*off >= len)
+        return 0;
+        
+    if (size > len - *off)
+        size = len - *off;
+        
+    read_lock(&lock);
+    if (copy_to_user(buff, test_string + *off, size)) {
+        read_unlock(&lock);
+        return -EFAULT;
+    }
+    read_unlock(&lock);
+    
+    *off += size;
+    return size;
+}
+
+static ssize_t test_write(struct file *file, const char __user *buff, size_t size, loff_t *off)
+{
     if (size > 15)
         return -EINVAL;
-
+        
     write_lock(&lock);
-    rc = simple_write_to_buffer(test_string, 15, off, buff, size);
+    if (copy_from_user(test_string, buff, size)) {
+        write_unlock(&lock);
+        return -EFAULT;
+    }
+    test_string[size] = '\0';
     write_unlock(&lock);
-
-    return rc;
+    
+    return size;
 }
 
 static struct file_operations fops = {
     .owner = THIS_MODULE,
-    .read = test_read,
+    .llseek = test_llseek,
+    .open  = test_open,
+    .read  = test_read,
     .write = test_write
 };
 
-int init_module(void)
+static int __init test_init(void)
 {
-    pr_info("Test module is loaded.\n");
+    pr_info("Test module loading...\n");
     rwlock_init(&lock);
-    major = register_chrdev(major, "test2", &fops);
-
-    if(major < 0)
+    major = register_chrdev(0, "test2", &fops);
+    if (major < 0) {
+        pr_err("register_chrdev failed\n");
         return major;
-   
-    pr_info("Major number is %d.\n", major);
-
+    }
+    pr_info("Major number: %d\n", major);
     return 0;
 }
 
-void cleanup_module(void)
+static void __exit test_exit(void)
 {
-    pr_info("Test module is unloaded.\n");
     unregister_chrdev(major, "test2");
+    pr_info("Test module unloaded\n");
 }
 
+module_init(test_init);
+module_exit(test_exit);
 MODULE_LICENSE("GPL");
 
